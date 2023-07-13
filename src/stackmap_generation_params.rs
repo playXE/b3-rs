a1527416b77ce9dea5eb4d6f7bcec16245efc1dd
playXE <adel.prokurov@gmail.com>
2023-07-13 10:19:32 +0400

    add mutable access to code in stackmap generation

--- src/stackmap_generation_params.rs
+++ src/stackmap_generation_params.rs
@@ -142,7 +142,12 @@ impl<'a, 'b, 'c> StackmapGenerationParams<'a, 'b, 'c> {
         next_block == successor.0
     }
 
+    
     pub fn code(&self) -> &Code<'_> {
+        self.context.code
+    }
+
+    pub fn code_mut(&mut self) -> &mut Code<'_> {
         self.context.code
     }
 
